e4f30228e1db2be9cb2a1a16c4cc483352d03e9d
BUMBAIYA <amitchauhan263871@gmail.com>
2023-07-04 23:32:58 +0530

    created logo for the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.svg" type="image/image+xml" />
+      </Head>
       <body>
         <Main />
         <NextScript />
