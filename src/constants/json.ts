a4ed62b95cb3c22234ddbdc106bcbd7f649ec73d
BUMBAIYA <amitchauhan263871@gmail.com>
2023-07-09 21:54:21 +0530

    replaced string literal with json object freeze

--- src/constants/json.ts
+++ src/constants/json.ts
@@ -1,41 +1,39 @@
-export const JSON_TEMPLATE = `{
-  "squadName": "Super hero squad",
-  "homeTown": "Metro City",
-  "formed": 2016,
-  "secretBase": "Super tower",
-  "active": true,
-  "members": [
+const sampleJson = Object.freeze({
+  squadName: "Super hero squad",
+  homeTown: "Metro City",
+  formed: 2016,
+  secretBase: "Super tower",
+  active: true,
+  members: [
     {
-      "name": "Molecule Man",
-      "age": 29,
-      "secretIdentity": "Dan Jukes",
-      "powers": [
-        "Radiation resistance",
-        "Turning tiny",
-        "Radiation blast"
-      ]
+      name: "Molecule Man",
+      age: 29,
+      secretIdentity: "Dan Jukes",
+      powers: ["Radiation resistance", "Turning tiny", "Radiation blast"],
     },
     {
-      "name": "Madame Uppercut",
-      "age": 39,
-      "secretIdentity": "Jane Wilson",
-      "powers": [
+      name: "Madame Uppercut",
+      age: 39,
+      secretIdentity: "Jane Wilson",
+      powers: [
         "Million tonne punch",
         "Damage resistance",
-        "Superhuman reflexes"
-      ]
+        "Superhuman reflexes",
+      ],
     },
     {
-      "name": "Eternal Flame",
-      "age": 1000000,
-      "secretIdentity": "Unknown",
-      "powers": [
+      name: "Eternal Flame",
+      age: 1000000,
+      secretIdentity: "Unknown",
+      powers: [
         "Immortality",
         "Heat Immunity",
         "Inferno",
         "Teleportation",
-        "Interdimensional travel"
-      ]
-    }
-  ]
-}`;
+        "Interdimensional travel",
+      ],
+    },
+  ],
+});
+
+export const JSON_TEMPLATE = JSON.stringify(sampleJson, null, 2);
