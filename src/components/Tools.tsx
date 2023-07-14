49befc58c310c5021a1a1132de3990cdf45a0a3c
BUMBAIYA <amitchauhan263871@gmail.com>
2023-07-15 01:36:59 +0530

    added hotkeys to toolbar

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,14 +1,22 @@
 import { FocusIcon, ZoomInIcon, ZoomOutIcon } from "@/components/Icons";
 import { useTree } from "@/store/useTree";
+import { useHotkeys } from "@/hooks/useHotKeys";
 
 export default function Tools() {
   const centerView = useTree((state) => state.centerView);
   const zoomIn = useTree((state) => state.zoomIn);
   const zoomOut = useTree((state) => state.zoomOut);
+
+  useHotkeys([
+    ["mod,shift,+", zoomIn],
+    ["mod,shift,-", zoomOut],
+    ["mod,shift,c", centerView],
+  ]);
+
   return (
     <div className="flex items-center gap-2">
       <button
-        className="h-8 w-8 rounded-md p-[5px] hover:bg-gray-200"
+        className="h-8 w-8 rounded-md border border-gray-300 p-[5px] text-gray-700 hover:bg-gray-200"
         type="button"
         aria-label="Zoom In"
         onClick={zoomIn}
@@ -16,7 +24,7 @@ export default function Tools() {
         <ZoomInIcon />
       </button>
       <button
-        className="h-8 w-8 rounded-md p-[5px] hover:bg-gray-200"
+        className="h-8 w-8 rounded-md border border-gray-300 p-[5px] text-gray-700 hover:bg-gray-200"
         type="button"
         aria-label="Zoom Out"
         onClick={zoomOut}
@@ -24,7 +32,7 @@ export default function Tools() {
         <ZoomOutIcon />
       </button>
       <button
-        className="h-8 w-8 rounded-md p-[5px] hover:bg-gray-200"
+        className="h-8 w-8 rounded-md border border-gray-300 p-[5px] text-gray-700 hover:bg-gray-200"
         type="button"
         aria-label="focus to center"
         onClick={centerView}
