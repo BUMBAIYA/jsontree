6eb3d8aada1abbecd680caca181feb263fef2ea5
BUMBAIYA <amitchauhan263871@gmail.com>
2024-03-13 12:11:48 +0530

    test command palette

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -307,3 +307,35 @@ export function ArrowPath() {
     </svg>
   );
 }
+
+export function CommandPaletteIcon() {
+  return (
+    <svg
+      stroke="none"
+      fill="currentColor"
+      aria-hidden="true"
+      height="100%"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="100%"
+    >
+      <path d="m6.354 8.04-4.773 4.773a.75.75 0 1 0 1.061 1.06L7.945 8.57a.75.75 0 0 0 0-1.06L2.642 2.206a.75.75 0 0 0-1.06 1.061L6.353 8.04ZM8.75 11.5a.75.75 0 0 0 0 1.5h5.5a.75.75 0 0 0 0-1.5h-5.5Z"></path>
+    </svg>
+  );
+}
+
+export function SearchIcon() {
+  return (
+    <svg
+      stroke="none"
+      fill="currentColor"
+      aria-hidden="true"
+      height="100%"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="100%"
+    >
+      <path d="M10.68 11.74a6 6 0 0 1-7.922-8.982 6 6 0 0 1 8.982 7.922l3.04 3.04a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215ZM11.5 7a4.499 4.499 0 1 0-8.997 0A4.499 4.499 0 0 0 11.5 7Z"></path>
+    </svg>
+  );
+}
