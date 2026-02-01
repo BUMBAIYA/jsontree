54c870ff81135cde07fc55a0da1a43408cdc4f8d
BUMBAIYA <amitchauhan263871@gmail.com>
2026-02-01 23:48:48 +0530

    fix: removed auto hover on parent array and object node properties
    1. TODO: Need to figure out hover to hover all the array objects on hover
    2. Need to choose pre calculated note id or on hover find outgoing node strategy
    3. Need to measure both strategy's performanace.

--- src/core/node/ObjectNode.tsx
+++ src/core/node/ObjectNode.tsx
@@ -2,38 +2,20 @@ import { FC, memo } from "react";
 import { CustomNodeProps } from "@/core/node";
 import { ForeignNodeWrapper } from "@/core/node/NodeComponents/ForeignNodeWrapper";
 import { TextRenderer } from "@/core/node/TextRenderer";
-import {
-  getKeyColor,
-  getValueColor,
-} from "@/core/node/NodeComponents/getColors";
+import { getValueColor } from "@/core/node/NodeComponents/getColors";
 import { classNames } from "@/utility/classNames";
-import { useTree } from "@/store/useTree";
-
-const isObjectOrArrayCount = (v: string) => /^[{\[]\d+[}\]]$/.test(v);
 
 const Node: FC<CustomNodeProps> = ({ node, x, y }) => {
   const { text, width, height, data } = node;
-  const centerOnNode = useTree((s) => s.centerOnNode);
-  const setHoveredNodeId = useTree((s) => s.setHoveredNodeId);
-  const hoveredNodeId = useTree((s) => s.hoveredNodeId);
   if (data.isEmpty) return null;
 
   return (
-    <ForeignNodeWrapper
-      isObject
-      width={width}
-      height={height}
-      nodeId={node.id}
-      isHighlighted={hoveredNodeId === node.id}
-    >
+    <ForeignNodeWrapper isObject width={width} height={height}>
       <span className="flex h-full items-center">
         <span className="flex flex-col" style={{ fontSize: "12px" }}>
           {text.map((val: any, idx: any) => {
             const keyStr = JSON.stringify(val[0]).replaceAll('"', "");
             const valStr = String(val[1]);
-            const targetId = data?.objectKeyTargets?.[val[0]];
-            const isClickable =
-              isObjectOrArrayCount(valStr) && typeof targetId === "string";
 
             return (
               <span
@@ -47,28 +29,10 @@ const Node: FC<CustomNodeProps> = ({ node, x, y }) => {
                 <span className="inline overflow-hidden text-ellipsis whitespace-nowrap text-[crimson] dark:text-[#5eb9eb]">
                   {keyStr}:{" "}
                 </span>
-                {isClickable ? (
-                  <button
-                    type="button"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      centerOnNode(targetId);
-                    }}
-                    onMouseEnter={() => setHoveredNodeId(targetId)}
-                    onMouseLeave={() => setHoveredNodeId(null)}
-                    className={classNames(
-                      "pointer-events-auto cursor-pointer rounded px-0.5 outline-none hover:bg-black/10 dark:hover:bg-white/10",
-                      getValueColor(valStr),
-                    )}
-                  >
-                    {valStr}
-                  </button>
-                ) : (
-                  <TextRenderer
-                    classNames={classNames(getValueColor(valStr))}
-                    innerText={valStr}
-                  />
-                )}
+                <TextRenderer
+                  classNames={classNames(getValueColor(valStr))}
+                  innerText={valStr}
+                />
               </span>
             );
           })}
